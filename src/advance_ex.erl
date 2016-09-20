91702f716be6bacb0060a4d4aadfa31031c2b256
HernanRivasAcosta <hernanrivasacosta@gmail.com>
2016-09-20 14:11:40 -0300

    improved some of the comments and added more examples

--- src/advance_ex.erl
+++ src/advance_ex.erl
@@ -1,12 +1,12 @@
 %%==============================================================================
-%% This example shows the different things you can do when using the advanced
-%% niffy configuration
+%% This example highlights some of the things you can do when using niffy with a
+%% more complex configuration
 %%==============================================================================
 
 -module(advance_ex).
 
--niffy(#{% The functions is the list of functions we need to niffify, the same
-         % list we use on our basic example
+-niffy(#{% The list of functions we need to niffify. It's the same we use on the
+         % basic example
          functions => [return_random/0],
          % You can add a variety of options that modify niffy in a lot of ways
          options   => [% For example, you can use less strict types to avoid the
@@ -21,7 +21,7 @@
                        % compilers. Please, if you ever use this, let me know. I
                        % just can't think of a reason for this feature to exist
                        {compiler, "gcc"}]}%,
-         % Finally, you can set the load, upgrade and unload functions here. All
+         % Finally, you can set the load, upgrade and unload functions. They are
          % optional of course
          %load      => on_load_nif/1,
          %upgrade   => on_upgrade_nif/1,
@@ -31,13 +31,14 @@
 -export([return_random/0]).
 
 %%==============================================================================
-% Here, we are returning ERL_NIF_TERM, this means that we don't want niffy to
-% do anything on the operators
+% Here, we are returning ERL_NIF_TERM, this means that we don't want niffy to do
+% anything on the return values. This is useful even if niffy by default ignores
+% all the returns that already use enif_make_*
 return_random() ->
   "#include <stdlib.h>
    ERL_NIF_TERM return_random()
    {
-     // Let's do the logarithm of the number, this shows how both stdlib.h and
-     // math.h are loaded successfully
+     // Let's do the logarithm of a random number, this means that both stdlib.h
+     // and math.h were imported successfully
      return enif_make_int(env, log(rand()));
    }".
